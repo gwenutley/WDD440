e92f0b6c0bf0b68463b2259f516508ea9ad6bb21
Gwen Utley <gutley@byupathway.edu>
2026-01-20 11:22:34 -0700

    fixes

--- finances/nextjs-dashboard/next.config.ts
+++ finances/nextjs-dashboard/next.config.ts
@@ -1,7 +1,11 @@
+/** @type {import('next').NextConfig} */
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
